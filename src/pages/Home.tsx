9dabb7e44048a0fe2fb0728f14ad6f58e9393dda
Tomoya <flyingfox193807@gmail.com>
2025-05-29 12:11:37 -0700

    created folders and files and base code

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <div>
+        <img src="" alt="My Image" />
+      </div>
+      <p>Since September 2024.</p>
+    </div>
+  )
+}
+
+export default Home
